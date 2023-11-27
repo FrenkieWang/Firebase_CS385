209296ebc274cfb2d628b57974833dca1cc62641
FrenkieWang <FrenkieWang21@gmail.com>
2023-11-27 22:50:55 +0000

    Remove the config information

--- src/fbconfig.js
+++ src/fbconfig.js
@@ -4,13 +4,7 @@ import { getFirestore, initializeFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEuBKX4iY3T4CEGmxAALzGl4AMLyOC2GI",
-  authDomain: "pallet2023-c5df9.firebaseapp.com",
-  projectId: "pallet2023-c5df9",
-  storageBucket: "pallet2023-c5df9.appspot.com",
-  messagingSenderId: "925087154703",
-  appId: "1:925087154703:web:50620860e294f5e7e2997a",
-  measurementId: "G-DQSMDEZGW6"
+
  // you will need to generate this within Firebase yourself. 
 };
 
